cbc466fa7c754dbc63451771ad9fd2bdf76e115a
Tom Hinton <tomjhinton@gmail.com>
2020-07-18 20:50:33 +0100

    refactor

--- index.js
+++ index.js
@@ -29,15 +29,7 @@ board.on('ready', () => {
 
   // Add led to REPL (optional)
   //board.repl.inject({ anode });
-  // let index = 0;
-  // Turn it on and set the initial color
-  // const rainbow = ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "8F00FF"];
-  // board.loop(1000, () => {
-  //    rgb.color(rainbow[index++]);
-  //    if (index === rainbow.length) {
-  //      index = 0;
-  //    }
-  //  });
+  
 
 
   io.sockets.on('connection', function (socket) {
